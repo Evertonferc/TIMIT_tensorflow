8d0691547536283f9383f242b0cf20929e2d6127
Evertonferc <evertonfernandes_tkd@hotmail.com>
2018-03-19 17:36:28 -0300

    Foi criada a função processing onde será colocado o caminho dos arquivos e chamará nistread e phonemeread. E assim foi tirado as chamadas de diretórios destas duas funções.

--- complete.py
+++ complete.py
@@ -25,50 +25,88 @@ def all_files(root, patterns='*', single_level=False, yield_folders=False):
 
 ###função que abre os arquivos .wav, extrai as informações do header e le NIST em binário###
 
-def nistread():
-    for path in all_files('C:/Users/evert/Desktop/Testando/Alguns dados para teste/TRAIN/DR1/FCJF0', '*.wav'):
-        with open(path, 'rb') as wave:
-            bytes_read = wave.read()
-        wave.close()
+def nistread(path):
+    '''
+    Read a NIST file (it does not treat multi-channel files yet). 
+    It is based on TIMIT files avaliable in CD-ROM, which are 16 bits,
+    monoaural audio files.
+    
+    Parameter
+    ---------
+    path : str 
+        The full path of the directory that contain the files
+    
+
+    Returns
+    --------
+    signal : numpy.ndarray
+        Contain the bits of the signal on .wav file
+    fs : float
+        Its sampling rate    
+    bits : float
+        Its bits/sample (16 or 8)
+    version : float
+        Version of nist file
+    
+    '''
+    
+    
+    with open(path, 'rb') as wave:
+        bytes_read = wave.read()
+    wave.close()
+
+    # extraction and treatment of header
+    header = str(bytes_read[0:1023])
+
+    fs = float((re.search('(?<=sample_rate -i )\w+', header)).group(0))
+    bits = float((re.search('(?<=sample_sig_bits -i )\w+', header)).group(0))
+    version = float((re.search('(?<=database_version -s3 )\w+', header)).group(0))
 
 
-        # extraction and treatment of header
-        header = str(bytes_read[0:1023])
-
-        fs = float((re.search('(?<=sample_rate -i )\w+', header)).group(0))
-        bits = float((re.search('(?<=sample_sig_bits -i )\w+', header)).group(0))
-        version = float((re.search('(?<=database_version -s3 )\w+', header)).group(0))
+    if bits==16:
+        signal = np.array(array.array('h', bytes_read[1024:]))
+        return signal, fs, bits, version
 
 
-        if bits==16:
-            signal = np.array(array.array('h', bytes_read[1024:]))
-            return signal, fs, bits, version
-
-
-        elif bits==8:
-            signal = np.array(array.array('b', bytes_read[1024:]))
-            return signal, fs, bits, version
-        else:
-            print('this file is not in 16 bits nor 8 bits')
-
-
+    elif bits==8:
+        signal = np.array(array.array('b', bytes_read[1024:]))
+        return signal, fs, bits, version
+    else:
+        print('this file is not in 16 bits nor 8 bits')
 
 ###função que abre os arquivos .phn
 
-def phonemeread():
+def phonemeread(path):
+    '''
+    Read a PHN file; convert the begin and the end of respective phoneme in string
+    and organize in tuples.
     
-    for path in all_files('C:/Users/evert/Desktop/Testando/Alguns dados para teste/TRAIN/DR1/FCJF0', '*.PHN'):
-        with open(path, 'r') as text:
-           content = text.readlines()
-        text.close()
-        
-        spliting = [line.split() for line in content]
-
-        
-        t = [(int(lines[0]), int(lines[1]), lines[2]) for lines in spliting]
-
+    Parameters
+    ---------
+    path : str 
+        The full path of the directory that contain the files
     
-    return t
+
+    Returns
+    --------
+    phoneme : list of tuples
+        Contain tuples with 3 elements: int, int and str
+    
+    phoneme[0] = begin
+    phoneme[1] = end
+    phoneme[2] = phoneme
+    
+    '''
+    
+    with open(path, 'r') as text:
+       content = text.readlines()
+    text.close()
+        
+    spliting = [line.split() for line in content]
+    
+    phoneme = [(int(lines[0]), int(lines[1]), lines[2]) for lines in spliting]
+
+    return phoneme
 
 ### FORMAS DE USAR A FUNÇÃO PHONEMEREAD():
     
@@ -79,9 +117,22 @@ def phonemeread():
     
 #retorna uma lista com uma tupla com inicio, fim e o fonema
 
+#('C:/Users/evert/Desktop/Testando/Alguns dados para teste/TRAIN/DR1/FDML0', '*.wav')
 
-
-
+def processing(dirpath):
+    ###Recebe um caminho de diretório e le o nome de todos os arquivos .wav
+    ##muda a extensão da str .wav para .phn. Com as strings .wav ele alimenta
+    ###a função nistread, e com as strings .phn, ele alimenta a função phonemeread
+    ###faz um teste para saber quantos arquivos leu
+    
+    cont = 0 
+    for path in all_files(dirpath, '*.WAV'):
+        name_files = os.path.splitext(path)[0]
+        phonemestr = name_files + '.PHN'
+        a, b = nistread(path), phonemeread(phonemestr)
+        
+        cont += 1
+    return nistread(path), phonemeread(phonemestr)
 
 
 
