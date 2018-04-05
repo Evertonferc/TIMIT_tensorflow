bd7d08cb4f96d5016a753e3e28748d182609bf64
Evertonferc <31143348+Evertonferc@users.noreply.github.com>
2018-04-04 22:08:51 -0300

    Add files via upload

--- postes.py
+++ postes.py
@@ -15,19 +15,29 @@ def postes(n):
 #    elif x%i == 0 or x%i == 1:
         
     j = 40
-    while x%j != 0 and x%j < 1:
-        
-        j -= 1
+    r, t = 0,2
     
+#    while x%j != 0:
+#        
+#        j -= 1
+#    
     if j == 40 and x%j == 0:
         return print(int(x/40), 'postes com distância de', j, 'm entre os vãos')
-    elif j == 40:
-        l = x//40
-        r = round(x/(l+1),1)
-        t = x - r * l
-        return print(l, 'postes com distância de ', r, 'm entre os vãos e 1 ponte com distância ', round(t,1),'m do penultimo poste')
-    elif x%j == 0:
-        return print(int(x/j), 'postes com distância de ', j, 'm entre os vãos')
-    elif x%j == 1 and j != 40:
-        return print(int((x//j)-1), 'postes com distância de ', j, 'm entre os vãos e 1 ponte com distância ', round(j+1,1),'m do penultimo poste')
+    else:    
+        while abs(r - round(t,1)) > 1:
+            l = x//j
+            r = round(x/((x//j)+1),1)
+            t = x - r * l
+            
+            j -= 1
+            if r == t:
+                return print(l+1, 'postes com distância de ', r, 'm entre os vãos')
+            else:
+                return print(l, 'postes com distância de ', r, 'm entre os vãos e 1 ponte com distância de', round(t,1),'m do penultimo poste \n erro de:',abs(r-round(t,1)))
+#    elif x%j == 0:
+#        return print(int(x/j), 'postes com distância de ', j, 'm entre os vãos')
+#    elif x%j == 1 and j != 40:
+#        return print(int((x//j)-1), 'postes com distância de ', j, 'm entre os vãos e 1 ponte com distância ', round(j+1,1),'m do penultimo poste')
+    
+    
     
